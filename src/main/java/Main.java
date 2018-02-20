ab24f430a46a5ca31fdf30c919b7f7cf2c942b7f
xS0N3x <al315542@uji.es>
2018-02-20 12:25:42 +0100

    Generador de Datos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,11 @@ import es.uji.belfern.generador.GeneradorDatosINE;
 public class Main {
 
     public static void main(String[] args){
-        GeneradorDatosINE generador = new GeneradorDatosINE();
-        System.out.println(generador.getNombre());
-        System.out.println(generador.getApellido());
-        System.out.println(generador.getNIF());
+        for (int i = 0; i < 100; i++) {
+            GeneradorDatosINE generador = new GeneradorDatosINE();
+            System.out.println(generador.getNombre());
+            System.out.println(generador.getApellido());
+            System.out.println(generador.getNIF());
+        }
     }
 }
