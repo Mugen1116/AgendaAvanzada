88124dbc4173f12854a9c281041e77ad44af5860
xS0N3x <al315542@uji.es>
2018-04-10 12:25:13 +0200

    muchas cosas sin acabar

--- src/main/java/modelo/tarifa/TarifaTardes.java
+++ src/main/java/modelo/tarifa/TarifaTardes.java
@@ -7,11 +7,16 @@ import java.io.Serializable;
 public class TarifaTardes extends Tarifa implements Serializable{
 
     private static final long serialVersionUID = 4178039098460695702L;
-    public TarifaTardes(float precio){
-        super(precio);
+    public TarifaTardes(Tarifa tarifa, float precio){
+        super(tarifa, precio);
     }
 
     @Override public float getPrecioLlamada(Llamada llamada){
-        get
+        int horaLlamada = llamada.getDiaHora().getHours();
+        if ( horaLlamada >= 16 && horaLlamada < 20){
+            return getPrecio()*llamada.getDuracion();
+        }else{
+            return
+        }
     }
 }
